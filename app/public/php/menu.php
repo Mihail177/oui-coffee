bfc7967d7d0ef29b38ed725da8e5e1c3f67495d6
Mihail177 <mihail.josan@student.nhlstenden.com>
2023-10-20 16:55:02 +0200

    Add menu items and styling for it

--- app/public/php/menu.php
+++ app/public/php/menu.php
@@ -56,6 +56,50 @@ echo '
           <img src="ceai.png" alt="ceai" class="menuTrendingImgs" />
         </section>
       </article>
+      <h2 class="menuTrendingTitile">Le Menu</h2>
+      <article class="menuCategories">
+        <a href="#" class="menuCategoriesNames">Coffee</a>
+        <a href="#" class="menuCategoriesNames">Tea</a>
+        <a href="#" class="menuCategoriesNames">Pain</a>
+        <a href="#" class="menuCategoriesNames">Sweets</a>
+        <a href="#" class="menuCategoriesNames">Food</a>
+        <a href="#" class="menuCategoriesNames">Cool Drinks</a>
+      </article>
+      <article class="menuContainer">
+        <div class="menuItems menuSpanColumn2">
+          <h4>coffee</h4>
+          <p></p>
+        </div>
+        <div class="menuItems">
+          <div class="menuSubitems">
+            <h3>20% off</h3>
+            <p>ALL COFFEE DRINKS</p>
+          </div>
+          <a href="#" class="menuBtn">MORE INFORMATION</a>
+        </div>
+        <div class="menuItems menuSpanColumnFull">
+          <h4>tea</h4>
+        </div>
+        <div class="menuItems menuSpanRow2">
+          <div class="menuSubitems">
+            <h3>100%</h3>
+            <p>HANDMADE PRODUCTS</p>
+          </div>
+          <a href="#" class="menuBtn">ABOUT US</a>
+        </div>
+        <div class="menuItems menuSpanColumn2">
+          <h4>croissant (petite pain)</h4>
+        </div>
+        <div class="menuItems menuSpanColumn2">
+          <h4>sweets</h4>
+        </div>
+        <div class="menuItems menuSpanColumnFull">
+          <h4>food</h4>
+        </div>
+        <div class="menuItems menuSpanColumnFull">
+          <h4>cool drinks</h4>
+        </div>
+      </article>
     </article>
 
 ';
