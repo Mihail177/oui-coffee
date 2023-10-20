ef434c1c3cdd2663f42d1a80c52590981cdc6be1
Mihail177 <mihail.josan@student.nhlstenden.com>
2023-10-20 14:40:42 +0200

    ADD: Trending orders version1

---
+++ app/public/php/aboutUs.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="aboutsection.css">
+    <link href='https://fonts.googleapis.com/css?family=Caveat' rel='stylesheet'>
+    <title>Document</title>
+</head>
+<body>
+    <div class="aboutus-section">
+        <div class="aboutus-content">
+            <p id="aboutus-title">About us</p>
+            <p id="aboutus-text">We are a group of people who are very passionate about coffee and France. We love coffee, tea, biscuits, meringues, macarons, and many other pastries. We want to share our love for coffee with the world so we have brought teatime to you and everything has been made entirely by hand. From us to you. OUI.</p>
+        </div>
+    </div>  
+
+    <div id="map-section">
+        <div class="map">
+
+        </div>
+
+            <div class="timesandcontact">
+
+            </div>
+    </div>
+    
+</body>
+</html>
---
+++ app/public/php/menu.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="../css/style.css" />
+</head>
+</html>
+<?php
+function MenuSection(){
+echo '
+<h2 class="menuTrendingTitile">trending orders</h2>
+    <article class="menuView">
+      <article class="menuTrendingOrders">
+        <section class="menuTrendingItems">
+          <h6>👑Top of the day</h6>
+          <p>Rooibos Tea</p>
+          <hr />
+          <p>3.00</p>
+          <img src="ceai.png" alt="ceai" class="menuTrendingImgs" />
+        </section>
+        <section class="menuTrendingItems">
+          <h6>👑Top of the week</h6>
+          <p>French Ragout</p>
+          <hr />
+          <p>4.70</p>
+          <img src="ceai.png" alt="ceai" class="menuTrendingImgs" />
+        </section>
+        <section class="menuTrendingItems">
+          <h6>👑Top of the month</h6>
+          <p>Tuna Salad</p>
+          <hr />
+          <p>4.80</p>
+          <img src="ceai.png" alt="ceai" class="menuTrendingImgs" />
+        </section>
+        <section class="menuTrendingItems">
+          <h6>👑Top of the day</h6>
+          <p>Flat White</p>
+          <hr />
+          <p>4.20</p>
+          <img src="ceai.png" alt="ceai" class="menuTrendingImgs" />
+        </section>
+        <section class="menuTrendingItems">
+          <h6>👑Top of the week</h6>
+          <p>Macarons</p>
+          <hr />
+          <p>6.00</p>
+          <img src="ceai.png" alt="ceai" class="menuTrendingImgs" />
+        </section>
+        <section class="menuTrendingItems">
+          <h6>👑Top of the month</h6>
+          <p>Espresso</p>
+          <hr />
+          <p>1.32</p>
+          <img src="ceai.png" alt="ceai" class="menuTrendingImgs" />
+        </section>
+      </article>
+    </article>
+
+';
+}
+
+?>
