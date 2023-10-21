5048b0d4823236534d47d729975e0de5e89a14e0
OleksandrSemenovych <146714857+AlexD3F@users.noreply.github.com>
2023-10-21 22:52:37 +0300

    prototype of welcome(?) page
    Added css for welcome page and 1-st prototype(WIP) of it

---
+++ app/public/php/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+    <link rel="stylesheet" href="../css/style.css" />
+  </head>
+  <body>
+    <div class="header">
+      <div class="component-2">
+        <img class="pexels-chevanon-photography-33-icon" alt="" src=""/>
+      </div>
+      <div class="header-child"></div>
+      <div class="header-content">
+        <div class="welcome-parent">
+          <div class="welcome">welcome</div>
+          <div class="welcome1">welcome</div>
+        </div>
+        <div class="bienvenue">bienvenue!</div>
+        <img class="header-content-child" alt="" src="" />
+      </div>
+      <div class="no-bg-component">
+        <div class="logo-02-4-parent">
+          <img class="logo-02-4-icon" alt="" src="" />
+          <img class="logo-02-4-icon" alt="" src="" />
+          <img class="logo-02-6-icon" alt="" src="" />
+        </div>
+        <div class="le-menu-parent">
+          <div class="le-menu">Le Menu</div>
+          <div class="about-us">About Us</div>
+          <div class="about-us">Contact</div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
