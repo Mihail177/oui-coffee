a875f01d89cfc3ccc90eb20c42f7e7aa2c061762
OleksandrSemenovych <146714857+AlexD3F@users.noreply.github.com>
2023-10-24 17:41:09 +0300

    1.0 version of header
    1.0 version of header and header css file

--- app/public/php/header.php
+++ app/public/php/header.php
@@ -1,36 +1,33 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="initial-scale=1, width=device-width" />
-    <link rel="stylesheet" href="../css/style.css" />
-  </head>
-  <body>
-    <div class="header">
-      <div class="component-2">
-        <img class="pexels-chevanon-photography-33-icon" alt="" src=""/>
-      </div>
-      <div class="header-child"></div>
-      <div class="header-content">
-        <div class="welcome-parent">
-          <div class="welcome">welcome</div>
-          <div class="welcome1">welcome</div>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+      <div class="header1">
+        <div class="le-menu-parent"> <!--Moved here because it is the navbar-->
+            <img class="headerLogo" src="../imgs/logos/Logos-02.svg" alt="logo">
+            <div class="headerDropdown">
+                <div class="le-menu">Le Menu</div>
+                <div class="about-us">About Us</div>
+                <div class="about-us">Contact</div>
+            </div>
         </div>
-        <div class="bienvenue">bienvenue!</div>
-        <img class="header-content-child" alt="" src="" />
-      </div>
-      <div class="no-bg-component">
-        <div class="logo-02-4-parent">
-          <img class="logo-02-4-icon" alt="" src="" />
-          <img class="logo-02-4-icon" alt="" src="" />
-          <img class="logo-02-6-icon" alt="" src="" />
+        <div class="component-2"> <!--Makes sure there is no overflow-->
+            <img class="pexels-chevanon-photography-33-icon" alt="replace" src="../imgs/photos/pexels-chevanon-photography-333523.jpg">
         </div>
-        <div class="le-menu-parent">
-          <div class="le-menu">Le Menu</div>
-          <div class="about-us">About Us</div>
-          <div class="about-us">Contact</div>
+        <div class="header-child"></div>
+        <div class="header-content">
+            <h1 class="welcome">welcome</h1>
+            <h3 class="bienvenue">bienvenue!</h3>
+        <div class="no-bg-component">
+            <div class="logo-02-4-parent">
+            <img class="logo-02-2-icon" alt="replace" src="../imgs/illustrations/down-chevron-svgrepo-com.svg" />
+            <img class="logo-02-4-icon" alt="replace" src="../imgs/illustrations/down-chevron-svgrepo-com.svg" />
+            <img class="logo-02-6-icon" alt="replace" src="../imgs/illustrations/down-chevron-svgrepo-com.svg" />
+            <img class="logo-02-8-icon" alt="replace" src="../imgs/illustrations/down-chevron-svgrepo-com.svg" />
+            </div>
         </div>
       </div>
-    </div>
-  </body>
+    </body>
 </html>
