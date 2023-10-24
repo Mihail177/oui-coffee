195db047463a775f25e71f7a8de0f5426ac61a96
ioann0u <christos.ioannou@student.nhlstenden.com>
2023-10-24 20:29:24 +0200

    Add: map in map section + contact times

--- app/public/php/aboutUs.php
+++ app/public/php/aboutUs.php
@@ -17,10 +17,20 @@
 
     <div class="map-section">
         <div class="map">
-    <p> map</p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9652.936992847614!2d6.875193145720483!3d52.782084013645004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b7e66abc47f9b1%3A0x6faa0f0733f267f1!2sWildlands!5e0!3m2!1sen!2snl!4v1698171219096!5m2!1sen!2snl" width="800" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
 
             <div class="timesandcontact">
+            <h3> Available times </h3>
+            <p> Monday </p>
+            <p> Tuesday </p>
+            <p> Wednesday </p>
+            <p> Thursday </p>
+            <p> Friday </p>
+            <br>
+            <h3> Contact Us </h3>
+            <p> Email: chris@ioannou.xyz </p>
+            <p> Phone: 99990099 </p>
 
             </div>
     </div>
