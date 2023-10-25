9c5c5249223387beb5359d63cfe3cda06e630af9
ioann0u <christos.ioannou@student.nhlstenden.com>
2023-10-25 11:59:27 +0200

    Fix: camelCase

--- app/public/php/aboutUs.php
+++ app/public/php/aboutUs.php
@@ -8,32 +8,35 @@
     <title>Document</title>
 </head>
 <body>
-    <div class="aboutus-section">
-        <div class="aboutus-content">
-            <p id="aboutus-title">About us</p>
-            <p id="aboutus-text">We are a group of people who are very passionate about coffee and France. We love coffee, tea, biscuits, meringues, macarons, and many other pastries. We want to share our love for coffee with the world so we have brought teatime to you and everything has been made entirely by hand. From us to you. OUI.</p>
+    <div class="aboutusSection">
+        <div class="aboutusContent">
+            <p id="aboutusTitle">About us</p>
+            <p id="aboutusText">We are a group of people who are very passionate about coffee and France. We love coffee, tea, biscuits, meringues, macarons, and many other pastries. We want to share our love for coffee with the world so we have brought teatime to you and everything has been made entirely by hand. From us to you. OUI.</p>
         </div>
     </div>  
 
-    <div class="map-section">
+    <div class="mapSection">
         <div class="map">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9652.936992847614!2d6.875193145720483!3d52.782084013645004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b7e66abc47f9b1%3A0x6faa0f0733f267f1!2sWildlands!5e0!3m2!1sen!2snl!4v1698171219096!5m2!1sen!2snl" width="800" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9652.936992847614!2d6.875193145720483!3d52.782084013645004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b7e66abc47f9b1%3A0x6faa0f0733f267f1!2sWildlands!5e0!3m2!1sen!2snl!4v1698171219096!5m2!1sen!2snl" width="800" height="250" style="border:0;" allowfullscreen="false" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
 
-            <div class="timesandcontact">
+            <div class="timesContact">
             <h3> Available times </h3>
-            <p> Monday </p>
-            <p> Tuesday </p>
-            <p> Wednesday </p>
-            <p> Thursday </p>
-            <p> Friday </p>
-            <br>
+            <p> </p>
+            <p>Monday:&#160; &#160; &#160; &#160; &#160;10:00 - 14:00</p>
+            <p>Tuesday: &#160; &#160; &#160; &#160; &#160;09:00 - 19:00</p>
+            <p>Wednesday: &#160; &#160; &#160;09:00 - 19:00 </p>
+            <p>Thursday: &#160; &#160; &#160; &#160;12:00 - 17:00 </p>
+            <p>Friday: &#160; &#160; &#160; &#160; &#160;12:00 - 17:00 </p>
+            <p> </p>
+            <p> </p>
             <h3> Contact Us </h3>
+            <p> </p>
             <p> Email: chris@ioannou.xyz </p>
             <p> Phone: 99990099 </p>
-
-            </div>
+        </div>
+        
     </div>
-    <div class="copyrighttext">&copy; Copyright 2023 - OUI</div>
+    <div class="copyrightText">&copy; Copyright 2023 - OUI </div>
 </body>
 </html>
