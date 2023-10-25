f09303728223f0b4b72f59020a69dacdb2dd6e21
ioann0u <christos.ioannou@student.nhlstenden.com>
2023-10-25 19:26:38 +0200

    Fix: Map content + style

--- app/public/php/aboutUs.php
+++ app/public/php/aboutUs.php
@@ -15,28 +15,59 @@
         </div>
     </div>  
 
-    <div class="mapSection">
-        <div class="map">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9652.936992847614!2d6.875193145720483!3d52.782084013645004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b7e66abc47f9b1%3A0x6faa0f0733f267f1!2sWildlands!5e0!3m2!1sen!2snl!4v1698171219096!5m2!1sen!2snl" width="800" height="250" style="border:0;" allowfullscreen="false" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+<div class="mapSection">
+    <div class="map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3972.9945580207723!2d6.890963164390024!3d52.79529540920947!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNTLCsDQ3JzQwLjAiTiA2wrA1MyczOS4zIkU!5e0!3m2!1sen!2snl!4v1698239686776!5m2!1sen!2snl" width="750" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+    <div class="timesContact">
+        <h3>Available Days</h3>
+        <div class="bodyText">
+            <div class="daysAndTimes">
+                <div class="availableDays"> 
+                    <p>Monday:</p>
+                    <p>Tuesday:</p>
+                    <p>Wednesday:</p>
+                    <p>Thursday:</p>
+                    <p>Friday:</p>
+                </div>
+                <div class="availableTimes">
+                    <p>10:00 - 14:00 </p>
+                    <p>9:00 - 19:00 </p>
+                    <p>9:00 - 19:00 </p>    
+                    <p>12:00 - 17:00 </p>
+                    <p>12:00 - 17:00 </p>
+                </div>
+            </div>
+            <div class="contactUs">
+                <h3>Contact us: </h3>
+                <p>Email: administrator@oui.com</p>
+                <p>Phone: 99990099</p>
+            </div>
+        </div>
+    </div>
+    <div class="footerSection">
+        <div class="imageLogo"><img src="../imgs/logos/logo-02.png" alt=""></div>
+
+        <div class="quickLinks">
+            <h3> Quick Links </h3>
+            <p> </p>
+            <p>Home</p>
+            <p>About Us</p>
+            <p>Menu</p>
+            <p>Events</p>
+            <p>Location</p>
         </div>
 
-            <div class="timesContact">
-            <h3> Available times </h3>
-            <p> </p>
-            <p>Monday:&#160; &#160; &#160; &#160; &#160;10:00 - 14:00</p>
-            <p>Tuesday: &#160; &#160; &#160; &#160; &#160;09:00 - 19:00</p>
-            <p>Wednesday: &#160; &#160; &#160;09:00 - 19:00 </p>
-            <p>Thursday: &#160; &#160; &#160; &#160;12:00 - 17:00 </p>
-            <p>Friday: &#160; &#160; &#160; &#160; &#160;12:00 - 17:00 </p>
-            <p> </p>
-            <p> </p>
-            <h3> Contact Us </h3>
-            <p> </p>
-            <p> Email: chris@ioannou.xyz </p>
-            <p> Phone: 99990099 </p>
+        <div class="footerParagraph">
+            <p>We are a group of people who are very passionate about coffee and France. We love coffee, tea, biscuits, meringues, macarons, and many other pastries. We want to share our love for coffee with the world.</p></div>
         </div>
-        
+
+        <div class="footerContact">
+            <p>Email: administrator@oui.com</p>
+            <p>Phone: 99990099</p>
+        </div>
     </div>
-    <div class="copyrightText">&copy; Copyright 2023 - OUI </div>
+</div>
+    <div class="copyrightText">&copy; Copyright 2023 - OUI</div>
 </body>
 </html>
