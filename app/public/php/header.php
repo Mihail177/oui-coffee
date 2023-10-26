dd9273b3b4e181176e5a85e770aa3dd85f3202c8
OleksandrSemenovych <146714857+AlexD3F@users.noreply.github.com>
2023-10-26 21:07:22 +0300

    Prefinal Header part
    Most spare code remouved, remade tag names according to code of convention

--- app/public/php/header.php
+++ app/public/php/header.php
@@ -2,32 +2,37 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/style.css">
+        <link href='https://fonts.googleapis.com/css?family=Caveat' rel='stylesheet'>
     </head>
     <body>
-      <div class="header1">
-        <div class="le-menu-parent"> <!--Moved here because it is the navbar-->
-            <img class="headerLogo" src="../imgs/logos/Logos-02.svg" alt="logo">
-            <div class="headerDropdown">
-                <div class="le-menu">Le Menu</div>
-                <div class="about-us">About Us</div>
-                <div class="about-us">Contact</div>
+        <div class="HeaderMain">
+        <div class="HeaderLeMenuParent">
+            <img class="HeaderLogo" src="../imgs/logos/Logos-02.svg" alt="logo">
+            <div class="HeaderDropdown">
+                <h4>Le Menu</h4>
+                <h4>About Us</h4>
+                <h4>Contact</h4>
             </div>
         </div>
-        <div class="component-2"> <!--Makes sure there is no overflow-->
-            <img class="pexels-chevanon-photography-33-icon" alt="replace" src="../imgs/photos/pexels-chevanon-photography-333523.jpg">
+        <div> 
+            <img class="HeaderBackground HeaderWidth0 HeaderTop0 HeaderLeft0" alt="replace" src="../imgs/photos/pexels-chevanon-photography-333523.jpg">
         </div>
-        <div class="header-child"></div>
-        <div class="header-content">
-            <h1 class="welcome">welcome</h1>
-            <h3 class="bienvenue">bienvenue!</h3>
-        <div class="no-bg-component">
-            <div class="logo-02-4-parent">
-            <img class="logo-02-2-icon" alt="replace" src="../imgs/illustrations/down-chevron-svgrepo-com.svg" />
-            <img class="logo-02-4-icon" alt="replace" src="../imgs/illustrations/down-chevron-svgrepo-com.svg" />
-            <img class="logo-02-6-icon" alt="replace" src="../imgs/illustrations/down-chevron-svgrepo-com.svg" />
-            <img class="logo-02-8-icon" alt="replace" src="../imgs/illustrations/down-chevron-svgrepo-com.svg" />
+        <div class="HeaderChild HeaderWidth0"></div>
+        <div class="HeaderContent">
+            <h1 class="HeaderWelcome HeaderWidth0">welcome</h1> 
+            <h3 class="HeaderCoffeCL">It's coffe o'clock!</h3>  
+            <h3 class="HeaderBienvenue">bienvenue!</h3>   
+        </div>
+        <div class="HeaderNoBgComponent">
+            <div class="HeaderCorners">
+            <img class="HeadeDecorCornerRB HeaderRight0 HeaderBottom0" alt="replace" src="../imgs/illustrations/down-chevron-svgrepo-com.svg" />
+            <img class="HeadeDecorCornerLB HeaderLeft0 HeaderBottom0" alt="replace" src="../imgs/illustrations/down-chevron-svgrepo-com.svg" />
+            <img class="HeadeDecorCornerRT HeaderRight0 HeaderTop0" alt="replace" src="../imgs/illustrations/down-chevron-svgrepo-com.svg" />
+            <img class="HeadeDecorCornerLT HeaderLeft0 HeaderTop0" alt="replace" src="../imgs/illustrations/down-chevron-svgrepo-com.svg" />
             </div>
         </div>
-      </div>
+        </div>
     </body>
 </html>
