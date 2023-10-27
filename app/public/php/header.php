92ea6340fbbebbc3ac19192a7347a6102ddf942d
OleksandrSemenovych <146714857+AlexD3F@users.noreply.github.com>
2023-10-27 13:07:23 +0300

    PreFinal Headder v2
    Added changing background images, text was broken and be rewriten

--- app/public/php/header.php
+++ app/public/php/header.php
@@ -1,9 +1,15 @@
-<?php
-function header(){
-echo '
-        <div class="HeaderMain">
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/style.css">
+        <link href='https://fonts.googleapis.com/css?family=Caveat' rel='stylesheet'>
+    </head>
+    <body>
+        <div class="HeaderMain HeaderBody">
         <div class="HeaderLeMenuParent">
-            <img class="HeaderLogo" src="imgs/logos/Logos-02.svg" alt="logo">
+            <img class="HeaderLogo" src="../imgs/logos/Logos-02.svg" alt="logo">
             <div class="HeaderDropdown">
                 <h4>Le Menu</h4>
                 <h4>About Us</h4>
@@ -11,23 +17,22 @@ echo '
             </div>
         </div>
         <div> 
-            <img class="HeaderBackground HeaderWidth0 HeaderTop0 HeaderLeft0" alt="replace" src="imgs/photos/pexels-chevanon-photography-333523.jpg">
+            
         </div>
         <div class="HeaderChild HeaderWidth0"></div>
         <div class="HeaderContent">
-            <h1 class="HeaderWelcome HeaderWidth0">welcome</h1> 
-            <h3 class="HeaderCoffeCL">Its coffe oclock!</h3>  
-            <h3 class="HeaderBienvenue">bienvenue!</h3>   
+            <h1 class="HeaderWelcome HeaderCentring">welcome</h1> 
+            <h3 class="HeaderCoffeCL HeaderCentring">It's coffee o'clock!</h3>  
+            <h3 class="HeaderBienvenue HeaderCentring">bienvenue!</h3>   
         </div>
         <div class="HeaderNoBgComponent">
             <div class="HeaderCorners">
-            <img class="HeadeDecorCornerRB HeaderRight0 HeaderBottom0" alt="replace" src="imgs/illustrations/down-chevron-svgrepo-com.svg" />
-            <img class="HeadeDecorCornerLB HeaderLeft0 HeaderBottom0" alt="replace" src="imgs/illustrations/down-chevron-svgrepo-com.svg" />
-            <img class="HeadeDecorCornerRT HeaderRight0 HeaderTop0" alt="replace" src="imgs/illustrations/down-chevron-svgrepo-com.svg" />
-            <img class="HeadeDecorCornerLT HeaderLeft0 HeaderTop0" alt="replace" src="imgs/illustrations/down-chevron-svgrepo-com.svg" />
+            <img class="HeadeDecorCornerRB HeaderRight0 HeaderBottom0" alt="replace" src="../imgs/illustrations/down-chevron-svgrepo-com.svg" />
+            <img class="HeadeDecorCornerLB HeaderLeft0 HeaderBottom0" alt="replace" src="../imgs/illustrations/down-chevron-svgrepo-com.svg" />
+            <img class="HeadeDecorCornerRT HeaderRight0 HeaderTop0" alt="replace" src="../imgs/illustrations/down-chevron-svgrepo-com.svg" />
+            <img class="HeadeDecorCornerLT HeaderLeft0 HeaderTop0" alt="replace" src="../imgs/illustrations/down-chevron-svgrepo-com.svg" />
             </div>
         </div>
         </div>
-        ';
-    }
-?>
+    </body>
+</html>
