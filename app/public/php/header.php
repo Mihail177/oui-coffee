ac84f78c3935e1f40f3ce8419f8381c7b938c56f
Mihail177 <mihail.josan@student.nhlstenden.com>
2023-10-28 15:47:24 +0200

    Fixed the header part

--- app/public/php/header.php
+++ app/public/php/header.php
@@ -1,33 +1,47 @@
 <?php
 function headerSection(){
        echo '
-       <div class="HeaderMain HeaderBody">
-        <div class="HeaderLeMenuParent">
-            <img class="HeaderLogo" src="../imgs/logos/Logos-02.svg" alt="logo">
-            <div class="HeaderDropdown">
-                <h4>Le Menu</h4>
-                <h4>About Us</h4>
-                <h4>Contact</h4>
-            </div>
-        </div>
-        <div> 
-            
-        </div>
-        <div class="HeaderChild HeaderWidth0"></div>
-        <div class="HeaderContent">
-            <h1 class="HeaderWelcome HeaderCentring">welcome</h1> 
-            <h3 class="HeaderCoffeCL HeaderCentring">Its coffee oclock!</h3>  
-            <h3 class="HeaderBienvenue HeaderCentring">bienvenue!</h3>   
-        </div>
-        <div class="HeaderNoBgComponent">
-            <div class="HeaderCorners">
-            <img class="HeadeDecorCornerRB HeaderRight0 HeaderBottom0" alt="replace" src="../imgs/illustrations/down-chevron-svgrepo-com.svg" />
-            <img class="HeadeDecorCornerLB HeaderLeft0 HeaderBottom0" alt="replace" src="../imgs/illustrations/down-chevron-svgrepo-com.svg" />
-            <img class="HeadeDecorCornerRT HeaderRight0 HeaderTop0" alt="replace" src="../imgs/illustrations/down-chevron-svgrepo-com.svg" />
-            <img class="HeadeDecorCornerLT HeaderLeft0 HeaderTop0" alt="replace" src="../imgs/illustrations/down-chevron-svgrepo-com.svg" />
-            </div>
-        </div>
-        </div>
+       <header class="headerMain">
+       <div>
+         <img
+           src="imgs\down-chevron-svgrepo-com.svg"
+           alt="corner"
+           class="headerCorner headerCornerTL"
+         />
+         <img
+           src="imgs\down-chevron-svgrepo-com.svg"
+           alt="corner"
+           class="headerCorner headerCornerTR"
+         />
+         <img
+           src="imgs\down-chevron-svgrepo-com.svg"
+           alt="corner"
+           class="headerCorner headerCornerBL"
+         />
+         <img
+           src="imgs\down-chevron-svgrepo-com.svg"
+           alt="corner"
+           class="headerCorner headerCornerBR"
+         />
+       </div>
+       <nav class="headerNavBar">
+         <a href="">
+           <img class="headerLogo" src="imgs/logos/Logos-02.svg" alt="logo" />
+         </a>
+         <div class="headerBtn">
+           <a href="#" class="headerBtns">Le menu</a>
+           <a href="#" class="headerBtns">About Us</a>
+           <a href="#" class="headerBtns">Contact Us</a>
+         </div>
+       </nav>
+ 
+       <div class="headerText">
+         <h1 class="headerTitle">welcome</h1>
+         <p class="headerSubtitle">
+           bienvenue! its <span>coffee</span> oclock...
+         </p>
+       </div>
+     </header>
         ';
 }
 ?>
