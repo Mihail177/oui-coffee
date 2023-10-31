fc35a93fe866f48acb3effaa73d71c9a1907ff2f
Mihail177 <mihail.josan@student.nhlstenden.com>
2023-10-31 17:13:44 +0100

    Fixed everything in the about us and footer section in aboutUs.php

--- app/public/php/aboutUs.php
+++ app/public/php/aboutUs.php
@@ -1,65 +1,58 @@
 <?php
  function aboutUs(){
     echo'
-    <div class="aboutusSection">
-        <div class="aboutusContent">
-            <p id="aboutusTitle">About us</p>
-            <p id="aboutusText">We are a group of people who are very passionate about coffee and France. We love coffee, tea, biscuits, meringues, macarons, and many other pastries. We want to share our love for coffee with the world so we have brought teatime to you and everything has been made entirely by hand. From us to you. OUI.</p>
-        </div>
-    </div>  
-
-<div class="mapSection">
-    <div class="map">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3972.9945580207723!2d6.890963164390024!3d52.79529540920947!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNTLCsDQ3JzQwLjAiTiA2wrA1MyczOS4zIkU!5e0!3m2!1sen!2snl!4v1698239686776!5m2!1sen!2snl" width="750" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <section class="aboutusSection">
+    <div class="aboutusContent">
+        <h4 id="aboutusTitle">About us</h4>
+        <p id="aboutusText">We are a group of people who are very passionate about coffee and France. We love coffee, tea, biscuits, meringues, macarons, and many other pastries. We want to share our love for coffee with the world so we have brought teatime to you and everything has been made entirely by hand. From us to you. OUI.</p>
     </div>
-    <div class="timesContact">
-        <h3>Available Days</h3>
-        <div class="bodyText">
-            <div class="daysAndTimes">
-                <div class="availableDays"> 
-                    <p>Monday:</p>
-                    <p>Tuesday:</p>
-                    <p>Wednesday:</p>
-                    <p>Thursday:</p>
-                    <p>Friday:</p>
-                </div>
-                <div class="availableTimes">
-                    <p>10:00 - 14:00 </p>
-                    <p>9:00 - 19:00 </p>
-                    <p>9:00 - 19:00 </p>    
-                    <p>12:00 - 17:00 </p>
-                    <p>12:00 - 17:00 </p>
-                </div>
+</section>  
+
+<section class="aboutus"> 
+    <div class="aboutusLocation">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3972.9945580207723!2d6.890963164390024!3d52.79529540920947!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNTLCsDQ3JzQwLjAiTiA2wrA1MyczOS4zIkU!5e0!3m2!1sen!2snl!4v1698239686776!5m2!1sen!2snl" class="aboutusMap"></iframe>
+        
+        <div class="timesContact">
+        <h3 class="aboutusFooterTitle">Available Days</h3>
+            <div class="aboutusAvailability">
+                    <ul class="availableDays"> 
+                        <li>Monday:</li>
+                        <li>Tuesday:</li>
+                        <li>Wednesday:</li>
+                        <li>Thursday:</li>
+                        <li>Friday:</li>
+                    </ul>
+                    <ul class="availableTimes">
+                        <li>10:00 - 14:00 </li>
+                        <li>9:00  &nbsp;- 19:00 </li>
+                        <li>9:00  &nbsp;- 19:00 </li>    
+                        <li>12:00 - 17:00 </li>
+                        <li>12:00 - 17:00 </li>
+                    </ul>
             </div>
-            <div class="contactUs">
-                <h3>Contact us: </h3>
-                <p>Email: administrator@oui.com</p>
-                <p>Phone: 99990099</p>
-                <div class="contactButtons">
-                    <a href="www.facebook.com"><img src="imgs/logos/facebook.png" alt=""></a>
-                    <a href="www.instagram.com"><img src="imgs/logos/instagram.png" alt=""></a>
-                </div>
             </div>
         </div>
-    </div>
-    <div class="footerSection">
-        <img src="imgs/logos/logo-02.png" alt="">
+        <div class="aboutusLocation aboutusFooter">
+            <img src="imgs/logos/Logos-02.svg" alt="logo" class="footerLogo">
 
-        <div class="quickLinks">
-            <h3> Quick Links </h3>
-            <a href="#">Home</a>
-            <a href="#">About Us</a>
-            <a href="#">Menu</a>
-            <a href="#">Events</a>
-            <a href="#">Location</a>
-        </div>
-
-        <div class="footerParagraph">
-            <p>We are a group of people who are very passionate about coffee and France. We love coffee, tea, biscuits, meringues, macarons, and many other pastries. We want to share our love for coffee with the world.</p></div>
+            <div class="quickLinksBox">
+                <h3 class="aboutusFooterTitle"> Quick Links </h3>
+                <ul>
+                    <li><a href="#" class="quickLinksText">Home</a></li>
+                    <li><a href="#" class="quickLinksText">About Us</a></li>
+                    <li><a href="#" class="quickLinksText">Menu</a></li>
+                    <li><a href="#" class="quickLinksText">Events</a></li>
+                    <li><a href="#" class="quickLinksText">Location</a></li>
+                </ul>
+            </div>
+            <p class="footerParagraph">We are a group of people who are very passionate about coffee and France. We love coffee, tea, biscuits, meringues, macarons, and many other pastries. We want to share our love for coffee with the world.</p>
+            <ul class="contactUs">
+                <li>Email: administrator@oui.com</li>
+                <li>Phone: +3167541123444</li>
+            </ul>    
         </div>
     </div>
-</div>
-    <div class="copyrightText">&copy; Copyright 2023 - OUI</div>
+</section>
     ';
 };
     ?>
